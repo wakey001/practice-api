a9e4fae873f37fb3d62086fa43d63251bae20b03
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-04-21 16:48:58 +0000

    added postst form document

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+
+const API_KEY = "jZ1GhFL138_HxPMARBKNxBXaSn4";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        new Error(data.expiry)
+    }
+}
+
+function displayStatus(data) {
+    let heading = "API key status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+}
